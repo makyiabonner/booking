017f63042df23a36c9561bdc3ad21ed0366fa492
makyia bonner <makyiabonner@gmail.com>
2023-07-22 20:24:34 -0400

    Nav styling completed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,17 @@
-import 'Nav.module.scss';
+import styles from './Nav.module.scss';
 
 export default function Nav(){
     
     return (
-        <nav>
-            
-        </nav>
+        <nav className={styles.nav}>
+            <h1>Savvy</h1>
+            <div className={styles.div}>
+                <ul className={styles.ul}>
+                    <li><p className='p-0 m-0'>Home</p></li>
+                    <li><p className='p-0 m-0'>Booking</p></li>
+                </ul>
+                <a><button className='btn btn-secondary px-3 py-1 rounded'>Sign In</button></a>
+            </div>
+    </nav>
     )
 }
