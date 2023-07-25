48e2737b0539d55a37026daade18e0c3142b47bd
makyia bonner <makyiabonner@gmail.com>
2023-07-25 19:17:47 -0400

    added firstdiv image

--- src/components/HomeSection/HomeSection.js
+++ src/components/HomeSection/HomeSection.js
@@ -4,7 +4,14 @@ export default function HomeSection(){
     return (
         <div className={styles.div}>
             <main className={styles.main}>
-                <div className={styles.firstDiv}></div>
+                <div className={styles.firstDiv}>
+                    <div className={styles.imgContainer}>
+                    <img src='/images/continueBooking.jpg' 
+                         className={styles.img}
+                         alt='Continue last searched'
+                         />
+                    </div>
+                </div>
                 <div className='w-75 d-flex gap-5'>
                     <div className={styles.secondDiv}></div>
                     <div className={styles.thirdDiv}>
