167a1a4e17b8233dc44f9ed1a89d08a97a14f7ca
makyia bonner <makyiabonner@gmail.com>
2023-09-17 13:41:42 -0400

    optimized code

--- src/components/Homepage/HomeSection/HomeSection.js
+++ src/components/Homepage/HomeSection/HomeSection.js
@@ -5,19 +5,16 @@ import pic from '../../../../public/images/continueJourney.svg'
 export default function HomeSection(){
     return (
         <div className={styles.div}>
-            <main className={styles.main}>
+            <main className={styles.options_section}>
                 <div>
-                    <img src={pic} 
-                         className={styles.img}
-                         alt='Continue last searched'
-                         />
+                    <h1>Continue last searched</h1>
                 </div>
-                <div className={styles.second_row}>
-                    <div className={styles.secondDiv}>
-                        <h1>Coming Soon <br></br>To Mobile</h1>
+                <div className={styles.options_section_second_row}>
+                    <div>
+                        <h1>Coming Soon<br></br>To Mobile</h1>
                     </div>
-                    <div className={styles.thirdDiv}>
-                        <h1 className={styles.h1}>Create<br></br>Your<br></br>Account</h1>
+                    <div>
+                        <h1>Create<br></br>Your<br></br>Account</h1>
                     </div>
                 </div>
             </main>
