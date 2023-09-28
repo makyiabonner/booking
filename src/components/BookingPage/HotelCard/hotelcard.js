df4afc223d07a0176f5c14f5926a65de5ae6e256
makyia bonner <makyiabonner@gmail.com>
2023-09-28 16:10:18 -0400

    added scroll behavior to hotel results

--- src/components/BookingPage/HotelCard/hotelcard.js
+++ src/components/BookingPage/HotelCard/hotelcard.js
@@ -2,26 +2,24 @@ import styles from './hotelcard.module.scss';
 
 export default function HotelCard({img, name, location, price ,review}){
     return (
-        <div>
-            <div className={styles.card}>
-                <div className={styles.card_left}>
-                    <img 
-                        style={{borderRadius: '15px'}}
-                        src={img}
-                        width={200}
-                        height={150} 
-                    />
-                    <div className='d-flex flex-column'>
-                        <p className={styles.hotel_name}>{name}</p>
-                        <span className={styles.hotel_location}>{location}</span>
-                    </div>
+        <div className={styles.card}>
+            <div className={styles.card_left}>
+                <img 
+                    style={{borderRadius: '15px'}}
+                    src={img}
+                    width={200}
+                    height={150} 
+                />
+                <div className='d-flex flex-column'>
+                    <p className={styles.hotel_name}>{name}</p>
+                    <span className={styles.hotel_location}>{location}</span>
                 </div>
-                <div className={styles.card_right}>
-                    <p className={styles.hotel_rating}>{review}</p>
-                    <div>
-                        <span className={styles.hotel_nights}>nights, people</span>
-                        <p className={styles.hotel_pricing}>${price}</p>
-                    </div>
+            </div>
+            <div className={styles.card_right}>
+                <p className={styles.hotel_rating}>{review}</p>
+                <div>
+                    <span className={styles.hotel_nights}>nights, people</span>
+                    <p className={styles.hotel_pricing}>${price}</p>
                 </div>
             </div>
         </div>
