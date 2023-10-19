5127047d9e39a3e5a330070a871eee6422f9b8d7
makyia bonner <makyiabonner@gmail.com>
2023-10-19 10:10:50 -0400

    api key now hidden

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,10 @@
 
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
+
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'a9333e717fmsh3b7741a255b60d6p14f4b4jsn75e89e2dad03',
+		'X-RapidAPI-Key': apiKey,
     	'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
 	}
 };
